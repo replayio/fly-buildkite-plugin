927c418f2f158f1ec3037bbfe970f603b8694adf
Dan Miller <dan@replay.io>
2022-06-24 16:37:29 -0700

    Fix fly vs flyctl discrepancy

--- fly/proxy.ts
+++ fly/proxy.ts
@@ -37,7 +37,7 @@ export class FlyProxy {
   private startFlyProxy() {
     const flyProxy = Deno.run({
       cmd: [
-        "flyctl",
+        "fly",
         "--json",
         "--access-token",
         this.apiToken,
