4897d1f23501d57ec28f5d831375ebaa3cf51ba5
Dan Miller <dan@replay.io>
2022-07-05 16:39:32 -0700

    Revert "what happens if we pass --replace"
    This reverts commit eec38e52a15df66c7c8ed9ec268603d16eecd75a.

--- index.ts
+++ index.ts
@@ -150,7 +150,7 @@ async function main() {
 
   // pass pipelineString to build-agent pipeline upload stdin
   const p = Deno.run({
-    cmd: ["buildkite-agent", "pipeline", "upload", "--replace"],
+    cmd: ["buildkite-agent", "pipeline", "upload"],
     stdin: "piped",
   });
   await p.stdin.write(pipelineBytes);
